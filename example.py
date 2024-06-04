f66c1956298389ba8eae9283e70adaca8f8e505a
Fernando Guillen <fernandoguillen@Fernandos-MacBook-Pro.local>
2024-06-03 21:44:45 -0500

    Added Cache

--- example.py
+++ example.py
@@ -3,9 +3,13 @@ from typing import Tuple
 from urllib.parse import quote
 import subprocess
 import json
+from cachetools import cached, TTLCache
 
 # Crea una instancia de FastAPI
 app = FastAPI()
+
+cache = TTLCache(maxsize=128, ttl=3600)
+
 
 @app.get("/coordinates/{city}")
 def get_coordinates(city: str) -> Tuple[float, float]:
@@ -21,10 +25,12 @@ def get_coordinates(city: str) -> Tuple[float, float]:
     Raises:
     Exception: If the city is not found
     """
-    # Encode the city parameter
+    # Utiliza la caché para obtener las coordenadas si están disponibles
+    if city in cache:
+        return cache[city]
+
     encoded_city = quote(city)
 
-    # Uses the curl to make the API request
     curl_command = f'curl "https://nominatim.openstreetmap.org/search?q={encoded_city}&format=json"'
     result = subprocess.run(curl_command, shell=True, capture_output=True, text=True)
 
@@ -32,6 +38,8 @@ def get_coordinates(city: str) -> Tuple[float, float]:
     if data:
         latitude = float(data[0]["lat"])
         longitude = float(data[0]["lon"])
+
+        cache[city] = (latitude, longitude)
 
         return latitude, longitude
     else:
